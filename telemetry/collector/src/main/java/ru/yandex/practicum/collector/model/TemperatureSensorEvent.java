580985a26d89e89bd660f09a511a4b978b004f61
Your Name <your.email@example.com>
2025-11-21 17:32:52 +0500

    реализовал сервис Aggregator

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/model/TemperatureSensorEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/model/TemperatureSensorEvent.java
@@ -7,7 +7,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString(callSuper = true)
-public class TemperatureSensorEvent extends SensorEvent {
+public class  TemperatureSensorEvent extends SensorEvent {
     private Integer temperatureC;
     private Integer temperatureF;
 
